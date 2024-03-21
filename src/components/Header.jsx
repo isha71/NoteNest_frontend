9f1cbecd367bf720b09f6e45a4e8eafea40581d3
isha71 <ishu.akjain@gmail.com>
2024-03-21 11:38:40 +0530

    add hamburger menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { SERVER_ADDRESS } from "../config";
@@ -10,7 +10,7 @@ function Header(props) {
   axios.defaults.headers.common["Authorization"] = `Bearer ${currentUserToken}`; // Set authorization header with user token
   const navigate = useNavigate(); // Initializing useNavigate hook for navigation
 
-   // Function to handle user logout
+  // Function to handle user logout
   function handleLogOut() {
     localStorage.removeItem("userToken"); // Remove user token from local storage
     navigate("/"); // Redirect user to home page after logout
@@ -18,43 +18,89 @@ function Header(props) {
 
   // Function to handle user account deletion
   function handleDeleteAccount() {
-    if (window.confirm("Are you sure you want to delete you account?")) { // Ask for confirmation before account deletion
+    if (window.confirm("Are you sure you want to delete you account?")) {
+      // Ask for confirmation before account deletion
       axios // Make DELETE request to delete user endpoint
         .delete(`${SERVER_ADDRESS}/deleteUser`)
-        .then((response) => { // Handle successful account deletion response
+        .then((response) => {
+          // Handle successful account deletion response
           localStorage.removeItem("userToken"); // Remove user token from local storage
           navigate("/"); // Redirect user to home page after account deletion
         })
-        .catch((err) => { // Handle account deletion errors
+        .catch((err) => {
+          // Handle account deletion errors
           const { message } = err.response.data;
           alert(message); // Alert user with error message
         });
     }
   }
 
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
+    // <header className="columnAlignment">
+    //   <nav className="rowAlignment headerContainer">
+    //     <h1 className="rowAlignment">
+    //       <i className="material-icons alignSelfCenter">description</i>
+    //       {head}
+    //     </h1>
+    //     <span className="fill"></span>
+    //     <span className="greetUser">Welcome, {props.username}</span>
+    //     <div className="rowAlignment">
+    //       <button
+    //         onClick={handleLogOut}
+    //         className="accentButton darkAccentButton navButton"
+    //       >
+    //         Log out
+    //       </button>
+    //       <button
+    //         onClick={handleDeleteAccount}
+    //         className="accentButton transparentAccentButton light navButton"
+    //       >
+    //         Delete account
+    //       </button>
+    //     </div>
+    //   </nav>
+    // </header>
     <header className="columnAlignment">
-      <nav className="rowAlignment headerContainer">
-        <h1 className="rowAlignment">
-          <i className="material-icons alignSelfCenter">description</i>
-          {head}
-        </h1>
-        <span className="fill"></span>
-        <span className="greetUser">Welcome, {props.username}</span>
-        <div className="rowAlignment">
-          <button
-            onClick={handleLogOut}
-            className="accentButton darkAccentButton navButton"
-          >
-            Log out
-          </button>
-          <button
-            onClick={handleDeleteAccount}
-            className="accentButton transparentAccentButton light navButton"
-          >
-            Delete account
-          </button>
+      <nav className="rowColumnAlignment ">
+        <div className="rowColumnAlignment">
+          <h1 className="rowAlignment">
+            <i className="material-symbols-outlined alignSelfCenter">
+              description
+            </i>
+            {head}
+          </h1>
+          <span className="greetUser">Welcome, {props.username}</span>
         </div>
+        <span className="fill"></span>
+
+        <div
+          className="menu rowAlignment"
+          onClick={() => {
+            setMenuOpen(!menuOpen);
+          }}
+        >
+          <span class="material-symbols-outlined">menu</span>
+        </div>
+        <ul className={menuOpen ? "open" : ""}>
+          <li>
+            <button
+              onClick={handleLogOut}
+              className="accentButton darkAccentButton navButton"
+            >
+              Log out
+            </button>
+          </li>
+          <li>
+            <button
+              onClick={handleDeleteAccount}
+              className="accentButton transparentAccentButton light navButton"
+            >
+              Delete account
+            </button>
+          </li>
+        </ul>
       </nav>
     </header>
   );
